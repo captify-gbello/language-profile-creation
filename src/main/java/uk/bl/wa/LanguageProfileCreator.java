c0f94c065de2d5f4fe6c5f5da80736dbf80e370a
Andrew Jackson <Andrew.Jackson@bl.uk>
2017-08-01 16:33:04 +0100

    Added gdwiki source data and updated profile.

--- src/main/java/uk/bl/wa/LanguageProfileCreator.java
+++ src/main/java/uk/bl/wa/LanguageProfileCreator.java
@@ -6,9 +6,15 @@ package uk.bl.wa;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.optimaize.langdetect.i18n.LdLocale;
 import com.optimaize.langdetect.ngram.NgramExtractors;
 import com.optimaize.langdetect.profiles.LanguageProfile;
@@ -23,6 +29,32 @@ import com.optimaize.langdetect.text.TextObjectFactory;
  *
  */
 public class LanguageProfileCreator {
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    private static void parseWikipediaJson(TextObject inputText,
+            String jsonString)
+            throws JsonParseException, JsonMappingException, IOException {
+
+        @SuppressWarnings("unchecked")
+        Map<String, String> page = mapper.readValue(jsonString,
+                Map.class);
+        inputText.append(page.get("title"));
+        inputText.append(page.get("text"));
+        System.out.println("PAGE+ " + page.get("title"));
+    }
+
+    private static void parseWikipediaJsonFile(TextObject inputText,
+            String filename) throws UnsupportedEncodingException,
+            FileNotFoundException, IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(
+                new FileInputStream(filename), "UTF-8"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                inputText.append(line);
+                parseWikipediaJson(inputText, line);
+            }
+        }
+    }
 
     /**
      * @param args
@@ -37,7 +69,7 @@ public class LanguageProfileCreator {
         TextObject inputText = textObjectFactory.create();
         long count = 0;
         // Scan files...
-        for (File f : new File("profile-input").listFiles()) {
+        for (File f : new File("profile-input/text").listFiles()) {
             System.out.println("Reading " + f.getName());
             try (BufferedReader br = new BufferedReader(
                     new InputStreamReader(new FileInputStream(f), "UTF-8"))) {
@@ -50,12 +82,16 @@ public class LanguageProfileCreator {
             }
         }
         System.out.println("Read " + count + " lines.");
+        for (File f : new File("profile-input/gdwiki-json/AA").listFiles()) {
+            System.out.println("Reading " + f.getName());
+            parseWikipediaJsonFile(inputText, f.getAbsolutePath());
+        }
 
         // create the profile:
         LanguageProfile languageProfile = new LanguageProfileBuilder(
                 LdLocale.fromString("gd"))
                         .ngramExtractor(NgramExtractors.standard())
-                        .minimalFrequency(3) // adjust to tune profile size to
+                        .minimalFrequency(500) // adjust to tune profile size to
                                              // around 30KB
                         .addText(inputText).build();
 
